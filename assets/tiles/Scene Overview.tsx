428c80a020092e30ca11aeef28bde5fe0fd7b825
agk8055 <alangeokurian10@gmail.com>
2026-07-24 21:32:19 +0530

    Added assets to gitignore

--- assets/tiles/Scene Overview.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="Scene Overview" tilewidth="64" tileheight="64" tilecount="324" columns="18">
- <image source="images/Scene Overview.png" width="1200" height="1200"/>
- <tile id="0">
-  <properties>
-   <property name="IsKingdom" type="bool" value="true"/>
-  </properties>
- </tile>
-</tileset>
--- assets/tiles/mountain.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="mountain" tilewidth="64" tileheight="64" tilecount="45" columns="9">
- <image source="images/mountain.png" width="576" height="324"/>
-</tileset>
